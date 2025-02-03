10d0962b03ec1797f999d5a74a4d35ae38c64b4f
Uliana Bulygina <ulanabulygina5@gmail.com>
2025-02-03 17:25:37 +0500

    day-10 связи между таблицами в бд

--- database/migrations/2025_02_03_115826_create_employers_table.php
+++ database/migrations/2025_02_03_115826_create_employers_table.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('girls', function (Blueprint $table) {
+        Schema::create('employers', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -24,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('girls');
+        Schema::dropIfExists('employers');
     }
 };
