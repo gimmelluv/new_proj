06f79b6ae9520b6df132b2e3cbe676629f78ae96
Uliana Bulygina <ulanabulygina5@gmail.com>
2025-02-07 15:10:09 +0500

    day-18 Editing, Updating, and Deleting a Resource

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+//Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -15,16 +16,19 @@ Route::get('/jobs', function () {
     ]);
 });
 
+//Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+//Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+//Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -37,6 +41,46 @@ Route::post('/jobs', function () {
         'employer_id' => 1
     ]);
 
+    return redirect('/jobs');
+});
+
+//Edit (форма для редактирования задания с конкретным {id})
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+//Update (обновляем конкретную вакансию)
+Route::patch('/jobs/{id}', function ($id) {
+   //validate
+   request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+   //authorize
+
+   //update the job
+   $job = Job::findOrFail($id);
+
+   $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+   ]);
+
+   //redirect to the job page
+   return redirect('/jobs/' . $job->id);
+});
+
+//Destroy (обновляем конкретную вакансию)
+Route::delete('/jobs/{id}', function ($id) {
+    //authorize
+
+    //delete the Job
+    Job::findOrFail($id)->delete();
+
+    //redirect
     return redirect('/jobs');
 });
 
