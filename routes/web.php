ef344ddce6577697ccc13f08d4a64bea5fe4cf66
Uliana Bulygina <ulanabulygina5@gmail.com>
2025-02-09 17:15:24 +0500

    day-21 login register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserContriller;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,3 +10,10 @@ Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+//auth
+Route::get('/register', [RegisteredUserContriller::class, 'create']);
+Route::post('/register', [RegisteredUserContriller::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
